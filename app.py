0c493c6e05e401afe6a2553c1ed3bbfa990a60b1
Kanishka-0506 <kanishkatamizh07@gmail.com>
2025-06-13 13:17:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -83,5 +83,8 @@ def texts():
     return render_template('texts.html')  
 # The duplicate about function has been removed.
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
